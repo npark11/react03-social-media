41186e40700f9d14247cac33f15a603452797980
Nari Park <npark11@myseneca.ca>
2022-11-04 16:14:34 -0400

    created login page design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,27 @@ import './login.scss';
 
 const Login = () => {
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga doloribus eos sint dolore quo, natus tempore similique animi quam architecto!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
