283b78647bbefcc6b24761394c16c0ec2e93547e
Nari Park <npark11@myseneca.ca>
2022-11-04 16:40:39 -0400

    created register page design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div>
-      <Login />
+      <Register />
     </div>
   );
 }
