4246b7ceaca5674b296c5b0c85dacd24bd38aaba
Nari Park <npark11@myseneca.ca>
2022-11-16 14:52:51 -0500

    edited left bar icons

--- src/components/leftBar/LeftBar.jsx
+++ src/components/leftBar/LeftBar.jsx
@@ -1,6 +1,23 @@
 import './leftBar.scss';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/authContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsersLine } from "@fortawesome/free-solid-svg-icons";
+import { faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
+import { faVideoCamera } from "@fortawesome/free-solid-svg-icons";
+import { faClapperboard } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { faImages } from "@fortawesome/free-solid-svg-icons";
+import { faStore } from "@fortawesome/free-solid-svg-icons";
+import { faStopwatch } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { faLandmark } from "@fortawesome/free-solid-svg-icons";
+import { faCubes } from "@fortawesome/free-solid-svg-icons";
+import { faChessBoard } from "@fortawesome/free-solid-svg-icons";
+
+
+
 
 const LeftBar = () => {
   
@@ -15,23 +32,23 @@ const LeftBar = () => {
             <span>{currentUser.name}</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faHandHoldingHeart} size="xl" color="#ff9cbd" width="2rem" />
             <span>Friends</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faUsersLine} size="xl" color="#d4abff" width="2rem" />
             <span>Groups</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faStore} size="xl" color="#abd5ff" width="2rem" />
             <span>Marketplace</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faClapperboard} size="xl" color="#8c57ff" width="2rem" />
             <span>Watch</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faStopwatch} size="xl" color="#10d85d" width="2rem" />
             <span>Memories</span>
           </div>
         </div>
@@ -39,23 +56,23 @@ const LeftBar = () => {
         <div className="menu">
           <span>Your shortcuts</span>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+          <FontAwesomeIcon icon={faCalendarDays} size="xl" color="#67d6fa" width="2rem" />
             <span>Events</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faGamepad} size="xl" color="#d4abff" width="2rem" />
             <span>Gaming</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faImages} size="xl" color="#ff9d52" width="2rem" />
             <span>Gallery</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faVideoCamera} size="xl" color="#6a4bda" width="2rem" />
             <span>Videos</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faEnvelope} size="xl" color="#ff84e6" width="2rem" />
             <span>Messages</span>
           </div>
         </div>
@@ -63,15 +80,15 @@ const LeftBar = () => {
         <div className="menu">
           <span>Others</span>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faCubes} size="xl" color="#ff57f4" width="2rem" />
             <span>Fundraiser</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faChessBoard} size="xl" color="#00df65" width="2rem" />
             <span>Tutorials</span>
           </div>
           <div className="item">
-            <img src="https://picsum.photos/50/50" alt="" />
+            <FontAwesomeIcon icon={faLandmark} size="xl" color="#ff3747" width="2rem" />
             <span>Courses</span>
           </div>
         </div>
